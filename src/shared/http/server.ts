a0f0ad74dbf9b162bca08a6d7e7ab126325b484c
Ercio Alendre <ercio.alendre@gmail.com>
2021-07-29 04:06:19 -0300

    refactor: setting server.ts, routes folder and routes main file

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,10 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(routes);
+app.listen(3000, () => {
+  console.log('[api-vendas] Server is running at localhost:3000');
+});
