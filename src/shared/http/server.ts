e02e031da9939ff3db72564c3dbdd9d87e2b1dab
Ercio Alendre <ercio.alendre@gmail.com>
2021-07-30 04:23:18 -0300

    refactor: implementation of a custom error manager

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,10 +1,24 @@
-import express from 'express';
-import cors from 'cors';
-import routes from './routes';
+import express, { NextFunction, Request, Response } from "express";
+import cors from "cors";
+import routes from "./routes";
+import AppError from "@shared/errors/AppError";
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  } else {
+    return res.status(500).json({
+      status: "error",
+      message: "Internal server error",
+    });
+  }
+});
 app.listen(3000, () => {
-  console.log('[api-vendas] Server is running at localhost:3000');
+  console.log("[api-vendas] Server is running at localhost:3000");
 });
