0997e54a26ac94c179606dd76bb9a93daa50afe5
Ercio Alendre <ercio.alendre@gmail.com>
2021-07-30 04:33:21 -0300

    refactor: unused variable removed from server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, { Request, Response } from "express";
 import cors from "cors";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+app.use((error: Error, req: Request, res: Response) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
       status: "error",
