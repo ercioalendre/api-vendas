389d4ec3e423c4cb2ee42fee7fe6100bfadcc01a
Ercio Alendre <ercio.alendre@gmail.com>
2021-07-31 02:37:58 -0300

    refactor: service to show a single product added

---
+++ src/modules/products/services/ShowProductService.ts
@@ -0,0 +1,24 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "@repositories/ProductsRepository";
+import Product from "@entities/Product";
+import AppError from "@shared/errors/AppError";
+
+interface IRequest {
+  id: string;
+}
+
+class ShowProductService {
+  public async execute({ id }: IRequest): Promise<Product | undefined> {
+    const productRepository = getCustomRepository(ProductRepository);
+
+    const product = productRepository.findOne(id);
+
+    if (!product) {
+      throw new AppError("The product you were looking for wasn't found.");
+    }
+
+    return product;
+  }
+}
+
+export default ShowProductService;
