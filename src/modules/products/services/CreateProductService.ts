729b50ac7636ec9d0d604e15e73c63447ff78693
Ercio Alendre <ercio.alendre@gmail.com>
2021-07-31 02:23:36 -0300

    refactor: service to create new products added

---
+++ src/modules/products/services/CreateProductService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from "typeorm";
+import { ProductRepository } from "@repositories/ProductsRepository";
+import AppError from "@shared/errors/AppError";
+import Product from "@entities/Product";
+
+interface IRequest {
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+class CreateProductService {
+  public async execute({ name, price, quantity }: IRequest): Promise<Product> {
+    const productRepository = getCustomRepository(ProductRepository);
+    const productExists = await productRepository.findByName(name);
+
+    if (productExists) {
+      throw new AppError("A product with that name already exists.");
+    }
+
+    const product = productRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await productRepository.save(product);
+
+    return product;
+  }
+}
+
+export default CreateProductService;
