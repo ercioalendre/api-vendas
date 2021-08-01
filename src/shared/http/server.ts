698f49322e7a4073849f7a4aa730f34d82679584
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-01 05:31:49 -0300

    refactor: errors from middleware celebrate implemented on server.ts

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import express, { NextFunction, Request, Response } from "express";
 import "express-async-errors";
 import cors from "cors";
+import { errors as celebrateErrors } from "celebrate";
 import routes from "./routes";
 import AppError from "@shared/errors/AppError";
 import "@shared/typeorm";
@@ -9,6 +10,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(routes);
+app.use(celebrateErrors());
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
   if (error instanceof AppError) {
     return res.status(error.statusCode).json({
