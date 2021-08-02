e1d8177d652122a53b766dcd46a1769fd340170e
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-02 04:20:13 -0300

    refactor: column option 'nullable: true' is necessary on TypeORM Users entity

--- src/modules/users/typeorm/entities/User.ts
+++ src/modules/users/typeorm/entities/User.ts
@@ -10,22 +10,18 @@ import {
 class User {
   @PrimaryGeneratedColumn("uuid")
   id: string;
-
   @Column()
   name: string;
-
   @Column()
   email: string;
-
   @Column()
   password: string;
-
-  @Column()
+  @Column({
+    nullable: true,
+  })
   avatar: string;
-
   @CreateDateColumn()
   created_at: Date;
-
   @UpdateDateColumn()
   updated_at: Date;
 }
