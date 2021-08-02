29efa42cfbfca3c79c938ddc3e007d3006c87038
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-02 02:57:18 -0300

    refactor: setting users routes on main routes file

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,5 +1,7 @@
 import productsRouter from "@ProductsRoutes";
+import usersRouter from "@UsersRoutes";
 import { Router } from "express";
 const router = Router();
 router.use("/products", productsRouter);
+router.use("/users", usersRouter);
 export default router;
