1833ed5f3e07f173c9883755e7a208b52db502eb
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-02 02:14:49 -0300

    refactor: service to create new user created

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,33 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import User from "@UsersEntities";
+import UsersRepository from "@UsersRepositories";
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const emailExists = await usersRepository.findByEmail(email);
+
+    if (emailExists) {
+      throw new AppError("A user with that email already exists.");
+    }
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    });
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default CreateUserService;
