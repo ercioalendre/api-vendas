51bbe9b8219e1b11ba8fcce55e55007ff2d61546
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-02 02:55:59 -0300

    refactor: routes to users created

---
+++ src/modules/users/routes/users.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import UsersController from "@UsersController";
+import { celebrate, Segments, Joi } from "celebrate";
+
+const usersRouter = Router();
+const usersController = new UsersController();
+
+usersRouter.get("/", usersController.index);
+
+usersRouter.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
+
+export default usersRouter;
