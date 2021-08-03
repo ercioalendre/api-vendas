f13b8297ed79c97fc1d65cc8c81b62a622302215
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-03 03:48:12 -0300

    routes to deal with sessions imported to routes main file

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,7 +1,9 @@
-import productsRouter from "@ProductsRoutes";
+import sessionsRouter from "@SessionsRoutes";
 import usersRouter from "@UsersRoutes";
+import productsRouter from "@ProductsRoutes";
 import { Router } from "express";
 const router = Router();
 router.use("/products", productsRouter);
 router.use("/users", usersRouter);
+router.use("/sessions", sessionsRouter);
 export default router;
