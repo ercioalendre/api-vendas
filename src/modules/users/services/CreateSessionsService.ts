8128f6c7caacaf30d31bfb1bc2388c7b0a3213fe
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-03 05:54:48 -0300

    refactor: JWT token implemented on sessions service

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -3,17 +3,20 @@ import AppError from "@shared/errors/AppError";
 import User from "@UsersEntities";
 import UsersRepository from "@UsersRepositories";
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
 
 interface IRequest {
   email: string;
   password: string;
 }
 
+interface IResponse {
+  user: User;
+  token: string;
+}
+
 class CreateSessionsService {
-  public async execute({
-    email,
-    password,
-  }: IRequest): Promise<User | undefined> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
     const userPassword = user?.password || "";
@@ -26,7 +29,15 @@ class CreateSessionsService {
       );
     }
 
-    return user;
+    const token = sign({}, "3ef42d1353b256c8c1bd8120028c185c", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
