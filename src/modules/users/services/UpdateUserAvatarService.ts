f0d814487ec9c9b69775be86cefbd76b7e284124
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-04 06:21:48 -0300

    service to deal with users avatar created

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import User from "@UsersEntities";
+import UsersRepository from "@UsersRepositories";
+import uploadConfig from "@config/upload";
+import path from "path";
+import fs from "fs";
+
+interface IRequest {
+  userId: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ userId, avatarFilename }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError("User was not found.", 401);
+    }
+
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = fs.existsSync(userAvatarFilePath);
+
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+
+    user.avatar = avatarFilename;
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default UpdateUserAvatarService;
