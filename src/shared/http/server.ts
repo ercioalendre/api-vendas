9118fc69549b11f8ee4296ba553cda826baa0947
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-06 02:33:22 -0300

    refactor: server file was remodeled

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,30 +1,8 @@
-import "reflect-metadata";
-import express, { NextFunction, Request, Response } from "express";
-import "express-async-errors";
-import cors from "cors";
-import { errors as celebrateErrors } from "celebrate";
-import routes from "./routes";
-import AppError from "@shared/errors/AppError";
-import "@shared/typeorm";
-const app = express();
-app.use(express.json());
-app.use(cors());
-app.use(routes);
-app.use(celebrateErrors());
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
-  if (error instanceof AppError) {
-    return res.status(error.statusCode).json({
-      status: "error",
-      message: error.message,
-    });
-  }
-  console.log(error);
-  return res.status(500).json({
-    status: "error",
-    message: "Internal server error",
-  });
-});
-app.listen(3000, () => {
-  console.log("[api-vendas] Server is running at localhost:3000");
+import app from "./app";
+
+const listenPort = process.env.PORT || 3000;
+const appName = process.env.APP_NAME || "api-vendas";
+
+app.listen(listenPort, () => {
+  console.log(`[${appName}] Server is running on port ${listenPort}!`);
 });
