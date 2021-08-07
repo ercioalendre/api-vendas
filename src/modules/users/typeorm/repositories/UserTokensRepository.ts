7826192d0cf7cfc1b5bccbd2a97d700e6ebdc037
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-07 07:54:31 -0300

    chore: es-lint changes

--- src/modules/users/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -3,6 +3,7 @@ import UserToken from "@UsersEntities/UserToken";
 
 @EntityRepository(UserToken)
 class UserTokensRepository extends Repository<UserToken> {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async findByToken(token: string): Promise<UserToken | any> {
     const userToken = await this.findOne({
       where: {
@@ -13,6 +14,7 @@ class UserTokensRepository extends Repository<UserToken> {
     return userToken;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   public async generate(user_id: string): Promise<UserToken | any> {
     const userToken = await this.create({
       user_id,
