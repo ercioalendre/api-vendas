715bd29e386c55fc949bba93e339c897cbcc30b7
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-07 07:52:52 -0300

    service to reset user password created

---
+++ src/modules/users/services/email/ResetPasswordService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository } from "typeorm";
+import { isAfter, addHours } from "date-fns";
+import { hash } from "bcryptjs";
+import AppError from "@shared/errors/AppError";
+import UsersRepository from "@UsersRepositories/UsersRepository";
+import UserTokensRepository from "@UsersRepositories/UserTokensRepository";
+
+interface IRequest {
+  token: string;
+  password: string;
+}
+
+class ResetPasswordService {
+  public async execute({ token, password }: IRequest): Promise<void> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const userTokensRepository = getCustomRepository(UserTokensRepository);
+    const userToken = await userTokensRepository.findByToken(token);
+
+    if (!token) {
+      throw new AppError("User Token was not found.", 401);
+    }
+
+    const user = await usersRepository.findById(userToken.user_id);
+
+    if (!user) {
+      throw new AppError("User was not found.", 401);
+    }
+
+    const tokenCreatedAt = userToken.createdAt;
+    const isTokenExpired = isAfter(Date.now(), addHours(tokenCreatedAt, 2));
+
+    if (isTokenExpired) {
+      throw new AppError("Token expired.", 401);
+    }
+
+    user.password = await hash(password, 8);
+  }
+}
+
+export default ResetPasswordService;
