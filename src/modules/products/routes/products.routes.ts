102e238bd3312180f3fef45e274734a2dc904e61
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-07 08:04:45 -0300

    refactor: ProductsController renamed to ProductsControllers and controller folder of products renamed to controllers

--- src/modules/products/routes/products.routes.ts
+++ src/modules/products/routes/products.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import ProductsController from "@ProductsController";
+import ProductsController from "@ProductsControllers/ProductsController";
 import { celebrate, Segments, Joi } from "celebrate";
 
 const productsRouter = Router();
