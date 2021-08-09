62888ef1383a1e5e3c94e7bd85b3ba6a60953c60
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-09 05:46:59 -0300

    service to manage user password changing created

---
+++ src/modules/users/services/password/ChangePasswordService.ts
@@ -0,0 +1,59 @@
+import { getCustomRepository } from "typeorm";
+import UsersRepository from "@UsersRepositories/UsersRepository";
+import User from "@UsersEntities/User";
+import AppError from "@shared/errors/AppError";
+import { compare, hash } from "bcryptjs";
+
+interface IRequest {
+  userId: string;
+  currentPassword: string;
+  newPassword: string;
+  newPasswordConfirmation?: string;
+}
+
+class ChangePasswordService {
+  public async execute({
+    userId,
+    currentPassword,
+    newPassword,
+    newPasswordConfirmation,
+  }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findById(userId);
+
+    if (!user) {
+      throw new AppError("User was not found.", 401);
+    }
+
+    if (newPassword !== newPasswordConfirmation) {
+      throw new AppError(
+        "New password and new password confirmation doesn't match.",
+      );
+    }
+
+    if (newPassword && !currentPassword) {
+      throw new AppError(
+        "You must insert your current password in order to change it",
+      );
+    }
+
+    if (newPassword && currentPassword) {
+      const checkCurrentPassword = await compare(
+        currentPassword,
+        user.password,
+      );
+
+      if (!checkCurrentPassword) {
+        throw new AppError("Your current password is wrong.");
+      }
+
+      user.password = await hash(newPassword, 8);
+    }
+
+    await usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export default ChangePasswordService;
