9b9e3825055462036ef9bea62c179ce7eb5dac35
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-09 05:41:01 -0300

    password routes updates with new routes

--- src/modules/users/routes/password.routes.ts
+++ src/modules/users/routes/password.routes.ts
@@ -2,10 +2,28 @@ import { Router } from "express";
 import { celebrate, Segments, Joi } from "celebrate";
 import ForgottenPasswordController from "@UsersControllers/password/ForgottenPasswordController";
 import ResetPasswordController from "@UsersControllers/password/ResetPasswordController";
+import ChangePasswordController from "@UsersControllers/password/ChangePasswordController";
+import isAuthenticated from "@shared/http/middlewares/isAuthenticated";
 
 const PasswordRouter = Router();
+const changePasswordController = new ChangePasswordController();
 const forgottenPasswordController = new ForgottenPasswordController();
 const resetPasswordController = new ResetPasswordController();
+
+PasswordRouter.put(
+  "/change",
+  isAuthenticated,
+  celebrate({
+    [Segments.BODY]: {
+      currentPassword: Joi.string().required(),
+      newPassword: Joi.string().required(),
+      newPasswordConfirmation: Joi.string()
+        .required()
+        .valid(Joi.ref("newPassword")),
+    },
+  }),
+  changePasswordController.update,
+);
 
 PasswordRouter.post(
   "/forgotten",
