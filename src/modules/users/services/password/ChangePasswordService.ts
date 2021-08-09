79122ba0849af66d25b2cda3a9fbf9d1615a9146
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-09 06:16:15 -0300

    service to change users password is now returning only necessary data

--- src/modules/users/services/password/ChangePasswordService.ts
+++ src/modules/users/services/password/ChangePasswordService.ts
@@ -52,7 +52,10 @@ class ChangePasswordService {
 
     await usersRepository.save(user);
 
-    return user;
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password, created_at, updated_at, ...userProfileData } = user;
+
+    return userProfileData as User;
   }
 }
 
