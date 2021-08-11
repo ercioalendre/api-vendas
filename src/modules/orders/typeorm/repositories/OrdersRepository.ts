05866fe25cc456145134ffe74ff2ee0b82ecb7b2
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-11 09:48:48 -0300

    custom repository for orders module created

---
+++ src/modules/orders/typeorm/repositories/OrdersRepository.ts
@@ -0,0 +1,36 @@
+import { EntityRepository, Repository } from "typeorm";
+import Order from "@OrdersEntities/OrderEntity";
+import Customer from "@CustomersEntities/CustomerEntity";
+
+interface IProduct {
+  product_id: string;
+  price: number;
+  quantity: number;
+}
+
+interface IRequest {
+  customer: Customer;
+  products: IProduct[];
+}
+
+@EntityRepository(Order)
+export default class OrdersRepository extends Repository<Order> {
+  public async findById(id: string): Promise<Order | undefined> {
+    const order = this.findOne(id, {
+      relations: ["order_products", "customer"],
+    });
+
+    return order;
+  }
+
+  public async createOrder({ customer, products }: IRequest): Promise<Order> {
+    const order = this.create({
+      customer,
+      order_products: products,
+    });
+
+    await this.save(order);
+
+    return order;
+  }
+}
