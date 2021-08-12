6b4f791dd0817f2f54a0a59c756c3ba518927acf
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-12 08:02:42 -0300

    orders routes added to the main routes file

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -5,14 +5,19 @@ import productsRouter from "@ProductsRoutes/products.routes";
 import uploadConfig from "@config/upload";
 import isAuthenticated from "@middlewares/isAuthenticated";
 import passwordRouter from "@UsersRoutes/password.routes";
-import profileRouter from "@modules/users/routes/profile.routes";
-import customersRouter from "@modules/customers/routes/customers.routes";
+import profileRouter from "@UsersRoutes/profile.routes";
+import customersRouter from "@CustomersRoutes/customers.routes";
+import ordersRouter from "@OrdersRoutes/orders.routes";
+
 const router = express.Router();
+
 router.use("/users", usersRouter);
 router.use("/sessions", sessionsRouter);
 router.use("/user/password", passwordRouter);
 router.use("/user/profile", profileRouter);
 router.use("/products", productsRouter);
 router.use("/customers", customersRouter);
+router.use("/orders", ordersRouter);
 router.use("/uploads", isAuthenticated, express.static(uploadConfig.directory));
+
 export default router;
