981d0a159f74321f2af03585923177b474467e64
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-12 07:47:15 -0300

    service to show order created

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,25 @@
+import { getCustomRepository } from "typeorm";
+import AppError from "@shared/errors/AppError";
+import Order from "@OrdersEntities/OrderEntity";
+import OrdersRepository from "@OrdersRepositories/OrdersRepository";
+
+interface IRequest {
+  id: string;
+}
+
+class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+
+    const order = await ordersRepository.findById(id);
+
+    if (!order) {
+      throw new AppError("The order you were looking for wasn't found.");
+    }
+
+    return order;
+  }
+}
+
+export const showOrderService = new ShowOrderService();
+export default showOrderService;
