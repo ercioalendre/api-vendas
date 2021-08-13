b030cae944a9db04819cd23d0297b4ddeceaa86a
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-13 09:43:54 -0300

    typeorm-pagination added to list customers service

--- src/modules/customers/services/ListCustomersService.ts
+++ src/modules/customers/services/ListCustomersService.ts
@@ -2,13 +2,24 @@ import { getCustomRepository } from "typeorm";
 import Customer from "@CustomersEntities/CustomerEntity";
 import CustomersRepository from "@CustomersRepositories/CustomersRepository";
 
+interface ICustomerPagination {
+  from: number;
+  to: number;
+  per_page: number;
+  total: number;
+  current_page: number;
+  prev_page: null | number;
+  next_page: null | number;
+  data: Customer[];
+}
+
 class ListCustomersService {
-  public async execute(): Promise<Customer[]> {
+  public async execute(): Promise<ICustomerPagination> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
-    const customers = customersRepository.find();
+    const customers = await customersRepository.createQueryBuilder().paginate();
 
-    return customers;
+    return customers as ICustomerPagination;
   }
 }
 
