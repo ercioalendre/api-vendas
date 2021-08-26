a92918942da264473199257c40030c04f22baa6c
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-26 00:27:05 -0300

    rate limiter main settings

---
+++ src/shared/http/middlewares/rateLimiter.ts
@@ -0,0 +1,30 @@
+import { Request, Response, NextFunction } from "express";
+import redis from "redis";
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import AppError from "@shared/errors/AppError";
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: Number(process.env.REDIS_PORT),
+  password: process.env.REDIS_PASSWORD || undefined,
+});
+
+const limiter = new RateLimiterRedis({
+  storeClient: redisClient,
+  keyPrefix: "ratelimit",
+  points: 5,
+  duration: 1,
+});
+
+export default async function rateLimiter(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): Promise<void> {
+  try {
+    await limiter.consume(req.ip);
+    return next();
+  } catch (err) {
+    throw new AppError("Too many requests.", 429);
+  }
+}
