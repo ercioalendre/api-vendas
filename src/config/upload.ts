f120feb60132b2d6bb465acf0f1f207715e4ea0d
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-28 10:55:12 -0300

    refactor: some data structure changes and unused imports removed

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,18 +1,44 @@
-import multer from "multer";
+import multer, { StorageEngine } from "multer";
 import path from "path";
-// import crypto from "crypto";
+
+interface IUploadSettings {
+  driver: "s3" | "local-disk";
+  tempFolder: string;
+  directory: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  settings: {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    disk: any;
+    aws: {
+      bucket: string;
+    };
+  };
+}
 
 const uploadFolder = path.resolve(__dirname, "..", "..", "uploads");
+const tempFolder = path.resolve(__dirname, "..", "..", "temp");
 
 export default {
+  driver: process.env.STORAGE_DRIVER,
+  tempFolder,
   directory: uploadFolder,
-  storage: multer.diskStorage({
-    destination: uploadFolder,
-    async filename(req, file, callback) {
-      const timestamp = new Date().getTime();
-      const [, fileExtension] = file.originalname.split(".");
-      const filename = `avatar-${req.user.id}-${timestamp}.${fileExtension}`;
-      callback(null, filename);
+  multer: {
+    storage: multer.diskStorage({
+      destination: tempFolder,
+      async filename(req, file, callback) {
+        const timestamp = new Date().getTime();
+        const [, fileExtension] = file.originalname.split(".");
+        const filename = `avatar-${req.user.id}-${timestamp}.${fileExtension}`;
+        callback(null, filename);
+      },
+    }),
+  },
+  settings: {
+    disk: {},
+    aws: {
+      bucket: process.env.AWS_S3_BUCKET,
     },
-  }),
-};
+  },
+} as IUploadSettings;
