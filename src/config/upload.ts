b77f49924cb57bb1ec78395e1a575227952d0bee
Ercio Alendre <ercio.alendre@gmail.com>
2021-08-28 23:37:39 -0300

    refactor: file size and mimo type verifications added to user avatar upload service

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,5 +1,6 @@
 import multer, { StorageEngine } from "multer";
 import path from "path";
+import AppError from "@shared/errors/AppError";
 
 interface IUploadSettings {
   driver: "s3" | "local-disk";
@@ -19,12 +20,20 @@ interface IUploadSettings {
 
 const uploadFolder = path.resolve(__dirname, "..", "..", "uploads");
 const tempFolder = path.resolve(__dirname, "..", "..", "temp");
+const maxFileSize = Number(process.env.UPLOAD_FILE_MAX_SIZE) * 1024 * 1024;
+const allowedMimes =
+  process.env.UPLOAD_ALLOWED_FILE_TYPES !== undefined
+    ? JSON.parse(process.env.UPLOAD_ALLOWED_FILE_TYPES)
+    : new AppError("Upload failed.");
 
 export default {
   driver: process.env.STORAGE_DRIVER,
   tempFolder,
   directory: uploadFolder,
   multer: {
+    limits: {
+      fileSize: maxFileSize,
+    },
     storage: multer.diskStorage({
       destination: tempFolder,
       async filename(req, file, callback) {
@@ -34,6 +43,18 @@ export default {
         callback(null, filename);
       },
     }),
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    fileFilter: (req: Request, file: any, callback: any) => {
+      if (allowedMimes.includes(file.mimetype)) {
+        callback(null, true);
+      } else {
+        callback(
+          new AppError(
+            "The file you are trying to send has an invalid extension.",
+          ),
+        );
+      }
+    },
   },
   settings: {
     disk: {},
