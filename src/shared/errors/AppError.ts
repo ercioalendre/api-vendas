ac3db78042768ef2e36d544c4dd9faaae61f6065
Ercio Alendre <ercio.alendre@gmail.com>
2021-09-01 14:45:02 -0300

    AppError class updated

--- src/shared/errors/AppError.ts
+++ src/shared/errors/AppError.ts
@@ -1,37 +1,54 @@
-import winston from "winston";
+// import winston from "winston";
 
-class AppError {
+// class AppError {
+//   public readonly message: string;
+//   public readonly statusCode: number;
+
+//   constructor(message: string, statusCode = 400) {
+//     this.message = message;
+//     this.statusCode = statusCode;
+
+//     const logSettings = {
+//       transports: [
+//         new winston.transports.File({
+//           filename: "logs/errors.log",
+//         }),
+//       ],
+//       format: winston.format.combine(
+//         winston.format.timestamp({
+//           format: "DD MMM YYYY HH:mm:ss",
+//         }),
+//         winston.format.printf(info => `${[info.timestamp]}: ${info.message}`),
+//       ),
+//     };
+
+//     const logger = winston.createLogger(logSettings);
+
+//     // Log a message
+//     logger.log({
+//       // Message to be logged
+//       message: this.message,
+
+//       // Level of the message logging
+//       level: "error",
+//     });
+//   }
+// }
+
+// export default AppError;
+
+class AppError extends Error {
   public readonly message: string;
   public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400) {
+    super(message);
+
+    Object.setPrototypeOf(this, new.target.prototype);
     this.message = message;
     this.statusCode = statusCode;
-
-    const logSettings = {
-      transports: [
-        new winston.transports.File({
-          filename: "logs/errors.log",
-        }),
-      ],
-      format: winston.format.combine(
-        winston.format.timestamp({
-          format: "DD MMM YYYY HH:mm:ss",
-        }),
-        winston.format.printf(info => `${[info.timestamp]}: ${info.message}`),
-      ),
-    };
-
-    const logger = winston.createLogger(logSettings);
-
-    // Log a message
-    logger.log({
-      // Message to be logged
-      message: this.message,
-
-      // Level of the message logging
-      level: "error",
-    });
+    Error.captureStackTrace(this);
+    // console.log(message, statusCode);
   }
 }
 
